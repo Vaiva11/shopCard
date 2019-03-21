b478df90d8462bd6386ab3b00b69b063aff9a036
Vaiva Gribauskaite <vaiva.gribauskaite@gmail.com>
2019-03-21 19:18:40 +0200

    added full implementacion of redux-api-middleware

--- src/shop/actions.js
+++ src/shop/actions.js
@@ -1,5 +1,6 @@
 import { RSAA } from "redux-api-middleware";
 import * as types from "./actionTypes";
+import { mapProducts } from "./utils";
 
 export const toggleFavorite = id => ({
   type: types.TOGGLE_FAVORITE_PRODUCT,
@@ -19,10 +20,11 @@ export const getProducts = () => ({
       types.FETCH_PRODUCTS,
       {
         type: types.FETCH_PRODUCTS_SUCCESS,
+        payload: mapProducts,
       },
       {
         type: types.FETCH_PRODUCTS_FAILURE,
-        payload: () => "Something REALLY wrong",
+        payload: "Something REALLY wrong",
       },
     ],
   },
